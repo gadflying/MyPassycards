7207732480607f688e049f68a91b0774ce7d3d2b
andrkit <andrkit@users.noreply.github.com>
2015-07-06 16:44:45 +0800

    change colors

--- MyPassycards/app/src/main/java/com/lguipeng/notes/utils/AccountUtils.java
+++ MyPassycards/app/src/main/java/com/lguipeng/notes/utils/AccountUtils.java
@@ -12,9 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-/**
- * Created by lgp on 2015/5/29.
- */
+
 public class AccountUtils {
 
     private static final String EMAIL_TYPE = "com.android.email";
